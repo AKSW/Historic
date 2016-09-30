563632b5ed5a0d4fbe7413362016d72ebbd4bb17
1Raphael <raphael.boyer@inria.fr>
2016-09-30 14:39:46 +0200

    Add files via upload

--- RawPageSpliter.node.js
+++ RawPageSpliter.node.js
@@ -1,3 +1,10 @@
+/*
+Fabien Gandon, Raphael Boyer, Olivier Corby, Alexandre Monnin. Wikipedia editing history in DBpedia : extracting and publishing the encyclopedia editing activity as linked data. IEEE/WIC/ACM International Joint Conference on Web Intelligence (WI' 16), Oct 2016, Omaha, United States. <hal-01359575>
+https://hal.inria.fr/hal-01359575
+
+Fabien Gandon, Raphael Boyer, Olivier Corby, Alexandre Monnin. Materializing the editing history of Wikipedia as linked data in DBpedia. ISWC 2016 - 15th International Semantic Web Conference, Oct 2016, Kobe, Japan. <http://iswc2016.semanticweb.org/>. <hal-01359583>
+https://hal.inria.fr/hal-01359583
+*/
 
 var Readable = require('stream').Readable;
 var util = require('util');
